c58a56b0b9a9afdb9e49d16d560cb3a6f3332d2f
Brayandiagnosty <desarrollo@itvcolombia.com>
2026-09-24 19:51:30 -0500

    feat: se agrego conexion a una bd de mongodb y se agrego las configuraciones en settings.py para la conexion

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'hospital.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django',
+        'NAME': 'hospital',
+        'CLIENT': {
+            'host': 'mongodb://localhost:27017',
+        }
     }
 }
 
