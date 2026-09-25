f691576b0ab35d16f4329939f38e78b56e130beb
Brayandiagnosty <desarrollo@itvcolombia.com>
2026-09-24 19:54:39 -0500

    feat: se agrego las apps creadas al archivo settings.py

--- hospital/hospital/settings.py
+++ hospital/hospital/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clinicas',
+    'facturaciones',
+    'farmacias',
+    'gestiones_citas',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
